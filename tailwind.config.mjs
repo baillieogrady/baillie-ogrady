ba8a2013372ed3fe62907f8419ab2ac91bcbdb4b
Baillie O'Grady <baillieogrady@Baillies-MacBook-Pro.local>
2024-01-23 20:03:58 +0000

    Initial

---
+++ tailwind.config.mjs
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+		  colors: {
+			'blue' : '#0000ff'
+		  }
+		},
+		container: {
+		  padding: {
+			DEFAULT: '1.25rem',
+			lg: '1.875rem',
+		  },
+		  center: true
+		},
+		screens: {
+		  'sm': '640px',
+		  // => @media (min-width: 640px) { ... }
+	
+		  'md': '768px',
+		  // => @media (min-width: 768px) { ... }
+	
+		  'lg': '1024px',
+		  // => @media (min-width: 1024px) { ... }
+	
+		  'xl': '1366px',
+		  // => @media (min-width: 1280px) { ... }
+	
+		  '2xl': '1440px',
+		  // => @media (min-width: 1536px) { ... }
+		}
+	},
+	plugins: [],
+}
